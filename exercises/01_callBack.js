1bfa28ff9454a6e07eaf7deddcbd512ae06c8058
rudenick0309 <rudenick0309@gmail.com>
2020-04-27 19:12:27 +0900

    promiseConstructor

--- exercises/01_callBack.js
+++ exercises/01_callBack.js
@@ -18,32 +18,47 @@ const request = require("request");
 //  */
 
 // const getDataFromFile = undefined;
+// const getDataFromFile =function(filePath, callback) {
+//   fs.readFile(filePath, 'utf8', function(err, data) {
+//     if(err) {
+//       // console.dir(err);
+//       // err.code ='ENOENT'
+//       // console.log(err.code)
+//       // return 'ENOENT';
+//       return callback(err, null)
+//     } else {
+//       // let arrData = [].push(data)
+//       let test= data.split('')
+//       let newArr = test.filter( e => {
+//         return Number(e);
+//       })
+//       console.log('타입', newArr);
+//
+//       let newData = [];
+//       newData.push(data)
+//       console.log(newData);
+//       console.log('에러이데이터', data)
+//
+//        callback(null, newArr);
+//     }
+//   })
+// }
+//리팩토링
 const getDataFromFile =function(filePath, callback) {
   fs.readFile(filePath, 'utf8', function(err, data) {
     if(err) {
-      // console.dir(err);
-      // err.code ='ENOENT'
-      // console.log(err.code)
-      // return 'ENOENT';
       return callback(err, null)
     } else {
-      // let arrData = [].push(data)
-      let test= data.split('')
-      let newArr = test.filter( e => {
+      let splitData= data.split('')
+      let newArr = splitData.filter( e => {
         return Number(e);
       })
-      console.log('타입', newArr);
-
-      let newData = [];
-      newData.push(data)
-      console.log(newData);
-      console.log('에러이데이터', data)
-
-       callback(null, newArr);
+      // let newData = [];
+      // newData.push(data)
+      callback(null, newArr);
     }
   })
 }
-
 
 //
 // /**
@@ -52,21 +67,33 @@ const getDataFromFile =function(filePath, callback) {
 //  */
 
 // const getBodyFromGetRequest = undefined;
+// const getBodyFromGetRequest =function(url, callback) {
+//   request(url, function (err, body) {
+//     if(err && !body) {
+//       // console.log('Invalid URI "none"')
+//       return callback(err, null)
+//     } else if (!err && body) {
+//       // var data = JSON.parse(body);
+//       console.log('데이터 불린', Array.isArray(body))
+//       console.log('데이터 body', JSON.parse(body.body))
+//       // var body = body.body
+//       // console.log(JSON.stringify(body).body[0].name)
+//       return callback(null, JSON.parse(body.body))
+//     }
+//   })
+// };
+
+//리팩토링-콘솔 없애고, 리턴 없애고, 조건문 수정하고,
 const getBodyFromGetRequest =function(url, callback) {
   request(url, function (err, body) {
-    if(err && !body) {
-      // console.log('Invalid URI "none"')
-      return callback(err, null)
-    } else if (!err && body) {
-      // var data = JSON.parse(body);
-      console.log('데이터 불린', Array.isArray(body))
-      console.log('데이터 body', JSON.parse(body.body))
-      // var body = body.body
-      // console.log(JSON.stringify(body).body[0].name)
-      return callback(null, JSON.parse(body.body))
+    if(err) {
+      callback(err, null)
+    } else {
+      callback(null, JSON.parse(body.body))
     }
   })
 };
+
 
 module.exports = {
   getDataFromFile: getDataFromFile,
--- exercises/02_promiseConstructor.js
+++ exercises/02_promiseConstructor.js
@@ -16,12 +16,39 @@ const request = require("request");
  * callback 함수에 넘겨 줄 Array
  * ["윤상호", "구일모", "이호용", "정진석, 박준홍", "최규홍"]
  */
-const getDataFromFilePromise = filePath => {};
+const getDataFromFilePromise = filePath => {
+  return new Promise((resolve, reject) => {
+    return fs.readFile(filePath, 'utf8', function(err,data) {
+      if (err) {
+        reject(err)
+      } else {
+        let splitData= data.split('')
+        let newArr = splitData.filter( e => {
+          return Number(e);
+        })
+        console.log('프로미스', data)
+        resolve(newArr);
+      }
+    })
+  })
+};
 
 /**
  * 아래 함수는 항상 Http Response의 Body를 callback함수에 넘겨주어야 합니다.
  */
-const getBodyFromGetRequestPromise = url => {};
+const getBodyFromGetRequestPromise = url => {
+  return new Promise((resolve, reject) => {
+    request(url, function(err, body) {
+      if (err) {
+        reject(err)
+      } else {
+        console.log('바디 확인', JSON.parse(body.body))
+        resolve(JSON.parse(body.body))
+      }
+    })
+  })
+
+};
 
 module.exports = {
   getDataFromFilePromise,
